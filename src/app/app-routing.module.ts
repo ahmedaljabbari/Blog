7cc094368a01ea2a54fbe879f7310dbea5b2f5ca
ahmedaljabbari <ahmedaljabbari@outlook.com>
2020-09-01 15:08:24 +0200

    <app-routing.module.ts> fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/components/home/home.component';
+import { LatestComponent } from '../app/components/latest/latest.component';
+import { AllArticlesComponent } from '../app/components/all-articles/all-articles.component';
+import { NewArticleComponent } from '../app/components/new-article/new-article.component';
+import { LoginComponent } from '../app/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/components/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'latest', component: LatestComponent, pathMatch: 'full' },
+  { path: 'all-articles', component: AllArticlesComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'new-article', component: NewArticleComponent, pathMatch: 'full' },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
