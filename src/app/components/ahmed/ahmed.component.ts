57ec76cdef4c905d21d7c0c8e1c4613ae797ed65
ahmedaljabbari <ahmedaljabbari@outlook.com>
2020-09-01 13:58:30 +0200

    Initializing the project 'first step'

---
+++ src/app/components/ahmed/ahmed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ahmed',
+  templateUrl: './ahmed.component.html',
+  styleUrls: ['./ahmed.component.css']
+})
+export class AhmedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
