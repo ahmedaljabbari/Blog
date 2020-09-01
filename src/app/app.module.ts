a691c1b0f1b2e8b8d5ea3ca95833e0e1553921e1
ahmedaljabbari <ahmedaljabbari@outlook.com>
2020-09-01 14:14:14 +0200

    Components Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllArticlesComponent } from './components/all-articles/all-articles.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewArticleComponent } from './components/new-article/new-article.component';
+import { LatestComponent } from './components/latest/latest.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AllArticlesComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    NewArticleComponent,
+    LatestComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
