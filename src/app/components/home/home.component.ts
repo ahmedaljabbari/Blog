09fef478fe329564fb60105c65cb14cb9b9c7c50
ahmedaljabbari <ahmedaljabbari@outlook.com>
2020-09-02 03:52:03 +0200

    HomeComponent.ts dev

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleServiceService } from '../../article-service.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  base: boolean = false;
+  article: any = [{}]
 
-  constructor() { }
+  constructor(private ArticleService: ArticleServiceService) { }
 
   ngOnInit(): void {
+    this.article = this.ArticleService.callLatest()
   }
 
 }
