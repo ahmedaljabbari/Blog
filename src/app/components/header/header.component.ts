66bdaee2ba86deaab92e01da6491d2b11e4edd19
ahmedaljabbari <ahmedaljabbari@outlook.com>
2020-09-02 07:14:54 +0200

    <header.component.ts && login-service> dev

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../../login-service.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  isAuthenticated: boolean = false;
 
-  constructor() { }
+  callAuthenticated() {
+    this.isAuthenticated = !this.isAuthenticated;
+  };
+  log_out(){
+    this.isAuthenticated = false;
+    this.LoginService.setValue(false);
+    }
+
+  constructor(private LoginService: LoginServiceService) {}
 
   ngOnInit(): void {
+    this.LoginService.getValue().subscribe((value) => {
+      this.isAuthenticated = value;
+    });
   }
-
 }
--- src/app/login-service.service.ts
+++ src/app/login-service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
+  private loginStatus: BehaviorSubject<boolean>;
 
-  constructor() { }
+
+  constructor() {
+    this.loginStatus = new BehaviorSubject<boolean>(false);
+  }
+  setValue(newValue:boolean): void {
+    this.loginStatus.next(newValue);
+  }
+  getValue(): Observable<boolean> {
+    return this.loginStatus.asObservable();
+  }
 }
