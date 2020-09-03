98deda485b5648824aeeaa35db14d6e00fdbc69a
ahmedaljabbari <ahmedaljabbari@outlook.com>
2020-09-03 03:06:35 +0200

    All article component dev. #1

--- src/app/components/all-articles/all-articles.component.ts
+++ src/app/components/all-articles/all-articles.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleServiceService } from '../../article-service.service';
 
 @Component({
   selector: 'app-all-articles',
   templateUrl: './all-articles.component.html',
-  styleUrls: ['./all-articles.component.css']
+  styleUrls: ['./all-articles.component.css'],
 })
 export class AllArticlesComponent implements OnInit {
+  titles: any = [{}];
 
-  constructor() { }
+  constructor(private ArticleService: ArticleServiceService) {}
 
   ngOnInit(): void {
+    this.titles = this.ArticleService.callTitles();
   }
-
 }
